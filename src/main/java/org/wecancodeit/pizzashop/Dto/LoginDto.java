59106e683c54cdfa7d5039bd910c849050501aec
JohnMoseley <jrmno1@hotmail.com>
2023-11-08 11:56:21 -0500

    update

--- src/main/java/org/wecancodeit/pizzashop/Dto/LoginDto.java
+++ src/main/java/org/wecancodeit/pizzashop/Dto/LoginDto.java
@@ -3,4 +3,24 @@ package org.wecancodeit.pizzashop.dto;
 public class LoginDto {
     private String userId;
     private String password;
+    public LoginDto() {
+    }
+    public LoginDto(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
 }
--- src/main/java/org/wecancodeit/pizzashop/controllers/HomeController.java
+++ src/main/java/org/wecancodeit/pizzashop/controllers/HomeController.java
@@ -1,13 +1,25 @@
 package org.wecancodeit.pizzashop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.wecancodeit.pizzashop.dto.LoginDto;
 
 @Controller
 public class HomeController {
-    
+
     @GetMapping("/")
-    public String loginUser(){
-        
+    public String loginUser(Model model) {
+        model.addAttribute("loginDto", new LoginDto());
+        return "home/login";
+    }
+
+    @PostMapping("/login" )
+    public String loginMethod(@ModelAttribute LoginDto dto) {
+        return "redirect:/PizzaMenu";
     }
 }
